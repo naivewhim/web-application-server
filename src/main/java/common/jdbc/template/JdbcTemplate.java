a4522dfbe69a056ce2cc00fc245a84a9dceecf33
LIM <LIM@LIM-PC>
2017-06-25 10:58:58 +0900

    [7.2.2] 요구사항 해결
    - UserDao 에서 필요한 쿼리(update, findAll, findById) 만 호출해서 사용하도록 변경

--- src/main/java/common/jdbc/template/JdbcTemplate.java
+++ src/main/java/common/jdbc/template/JdbcTemplate.java
@@ -1,18 +1,96 @@
 package common.jdbc.template;
 
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
-public interface JdbcTemplate<T> {
-	public void update(String string) throws SQLException;
+import core.jdbc.ConnectionManager;
 
-	public List<T> findAll(String sql) throws SQLException;
+public abstract class JdbcTemplate<T> {
+	public void update(String sql) throws SQLException {
+		Connection con = null;
+		PreparedStatement pstmt = null;
 
-	public T findObject(String sql) throws SQLException;
+		try {
+			con = ConnectionManager.getConnection();
+			pstmt = con.prepareStatement(sql);
+			setValues(pstmt);
 
-	public T mapRow(ResultSet rs) throws SQLException;
+			pstmt.executeUpdate();
+		} finally {
+			if (pstmt != null) {
+				pstmt.close();
+			}
 
-	public void setValues(PreparedStatement pstmt) throws SQLException;
+			if (con != null) {
+				con.close();
+			}
+		}
+	}
+
+	public List<T> findAll(String sql) throws SQLException {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			con = ConnectionManager.getConnection();
+			pstmt = con.prepareStatement(sql);
+
+			rs = pstmt.executeQuery();
+
+			List<T> resultList = new ArrayList<T>();
+			while (rs.next()) {
+				resultList.add(mapRow(rs));
+			}
+
+			return resultList;
+		} finally {
+			if (rs != null) {
+				rs.close();
+			}
+			if (pstmt != null) {
+				pstmt.close();
+			}
+			if (con != null) {
+				con.close();
+			}
+		}
+	}
+
+	public T findObject(String sql) throws SQLException {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			con = ConnectionManager.getConnection();
+			pstmt = con.prepareStatement(sql);
+			setValues(pstmt);
+
+			rs = pstmt.executeQuery();
+
+			T resultObject = null;
+			if (rs.next()) {
+				resultObject = mapRow(rs);
+			}
+
+			return resultObject;
+		} finally {
+			if (rs != null) {
+				rs.close();
+			}
+			if (pstmt != null) {
+				pstmt.close();
+			}
+			if (con != null) {
+				con.close();
+			}
+		}
+	}
+
+	public abstract T mapRow(ResultSet rs) throws SQLException;
+
+	public abstract void setValues(PreparedStatement pstmt) throws SQLException;
 }
--- src/main/java/next/dao/UserDao.java
+++ src/main/java/next/dao/UserDao.java
@@ -1,57 +1,22 @@
 package next.dao;
 
-import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
 import common.jdbc.template.JdbcTemplate;
-import core.jdbc.ConnectionManager;
 import next.model.User;
 
 public class UserDao {
 	public void insert(User user) throws SQLException {
 		JdbcTemplate<User> insertJdbcTemplate = new JdbcTemplate<User>() {
 			@Override
-			public void update(String sql) throws SQLException {
-				Connection con = null;
-				PreparedStatement pstmt = null;
-
-				try {
-					con = ConnectionManager.getConnection();
-					pstmt = con.prepareStatement(sql);
-					setValues(pstmt);
-
-					pstmt.executeUpdate();
-				} finally {
-					if (pstmt != null) {
-						pstmt.close();
-					}
-
-					if (con != null) {
-						con.close();
-					}
-				}
-			}
-
-			@Override
 			public void setValues(PreparedStatement pstmt) throws SQLException {
 				pstmt.setString(1, user.getUserId());
 				pstmt.setString(2, user.getPassword());
 				pstmt.setString(3, user.getName());
 				pstmt.setString(4, user.getEmail());
-			}
-
-			@Override
-			public List<User> findAll(String sql) throws SQLException {
-				return null;
-			}
-
-			@Override
-			public User findObject(String sql) throws SQLException {
-				return null;
 			}
 
 			@Override
@@ -67,43 +32,11 @@ public class UserDao {
 	public void update(User user) throws SQLException {
 		JdbcTemplate<User> updateJdbcTemplate = new JdbcTemplate<User>() {
 			@Override
-			public void update(String sql) throws SQLException {
-				Connection con = null;
-				PreparedStatement pstmt = null;
-
-				try {
-					con = ConnectionManager.getConnection();
-					pstmt = con.prepareStatement(sql);
-					setValues(pstmt);
-
-					pstmt.executeUpdate();
-				} finally {
-					if (pstmt != null) {
-						pstmt.close();
-					}
-
-					if (con != null) {
-						con.close();
-					}
-				}
-			}
-
-			@Override
 			public void setValues(PreparedStatement pstmt) throws SQLException {
 				pstmt.setString(1, user.getPassword());
 				pstmt.setString(2, user.getName());
 				pstmt.setString(3, user.getEmail());
 				pstmt.setString(4, user.getUserId());
-			}
-
-			@Override
-			public List<User> findAll(String sql) throws SQLException {
-				return null;
-			}
-
-			@Override
-			public User findObject(String sql) throws SQLException {
-				return null;
 			}
 
 			@Override
@@ -119,54 +52,14 @@ public class UserDao {
 	public List<User> findAll() throws SQLException {
 		JdbcTemplate<User> selectJdbcTemplate = new JdbcTemplate<User>() {
 			@Override
-			public List<User> findAll(String sql) throws SQLException {
-				Connection con = null;
-				PreparedStatement pstmt = null;
-				ResultSet rs = null;
-				try {
-					con = ConnectionManager.getConnection();
-					pstmt = con.prepareStatement(sql);
-
-					rs = pstmt.executeQuery();
-
-					List<User> userList = new ArrayList<User>();
-					while (rs.next()) {
-						userList.add(mapRow(rs));
-					}
-
-					return userList;
-				} finally {
-					if (rs != null) {
-						rs.close();
-					}
-					if (pstmt != null) {
-						pstmt.close();
-					}
-					if (con != null) {
-						con.close();
-					}
-				}
-			}
-
-			@Override
 			public User mapRow(ResultSet rs) throws SQLException {
 				return new User(rs.getString("userId"), rs.getString("password"), rs.getString("name"),
 						rs.getString("email"));
 			}
 
 			@Override
-			public User findObject(String sql) throws SQLException {
-				return null;
-			}
-
-			@Override
-			public void update(String string) throws SQLException {
-			}
-
-			@Override
 			public void setValues(PreparedStatement pstmt) throws SQLException {
 			}
-
 		};
 
 		String sql = "SELECT userId, password, name, email FROM USERS";
@@ -184,46 +77,6 @@ public class UserDao {
 			public User mapRow(ResultSet rs) throws SQLException {
 				return new User(rs.getString("userId"), rs.getString("password"), rs.getString("name"),
 						rs.getString("email"));
-			}
-
-			@Override
-			public User findObject(String sql) throws SQLException {
-				Connection con = null;
-				PreparedStatement pstmt = null;
-				ResultSet rs = null;
-				try {
-					con = ConnectionManager.getConnection();
-					pstmt = con.prepareStatement(sql);
-					setValues(pstmt);
-
-					rs = pstmt.executeQuery();
-
-					User user = null;
-					if (rs.next()) {
-						user = mapRow(rs);
-					}
-
-					return user;
-				} finally {
-					if (rs != null) {
-						rs.close();
-					}
-					if (pstmt != null) {
-						pstmt.close();
-					}
-					if (con != null) {
-						con.close();
-					}
-				}
-			}
-
-			@Override
-			public void update(String string) throws SQLException {
-			}
-			
-			@Override
-			public List<User> findAll(String sql) throws SQLException {
-				return null;
 			}
 		};
 
